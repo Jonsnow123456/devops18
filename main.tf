777e86013cecf3767f07d69a5b04f9ec9ec5ece0
Jonsnow123456 <117187011+Jonsnow123456@users.noreply.github.com>
2025-06-18 12:50:00 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0454e52560c7f5c55"
+    image_id           = "ami-09e6f87a47903347c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "lastone"
+    key_name = "sample"
     tags = {
         Name = "DevOps"
     }
@@ -15,7 +15,7 @@ resource "aws_launch_template" "web_server_as" {
   resource "aws_elb" "web_server_lb"{
      name = "web-server-lb"
      security_groups = [aws_security_group.web_server.id]
-     subnets = ["subnet-024286466e258f339", "subnet-09558309acf4661a8"]
+     subnets = ["subnet-0b8f074043eebed8a", "subnet-08084be035ffd6e5b"]
      listener {
       instance_port     = 8000
       instance_protocol = "http"
