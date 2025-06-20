ee2288c0838b237ee484c3fb9dc959668133b500
Jonsnow123456 <117187011+Jonsnow123456@users.noreply.github.com>
2025-06-20 15:18:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_launch_template" "web_server_as" {
     image_id           = "ami-09e6f87a47903347c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t2.micro"
-    key_name = "sample"
+    key_name = "project"
      tag_specifications {
       resource_type = "instance"
     tags = {
